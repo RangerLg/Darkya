2c3f38a590b9b239e73775dad0d1877bde743aa3
RangerLg <nick.manko@yandex.ru>
2022-01-08 16:28:50 +0300

    Added menu

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("Test");
+    }
+
+    // Update is called once per frame
+
+}

c20010d9b6c2d1c38a2a173e0ace797ea5f357be
bobothesexybear <80481012+bobothesexybear@users.noreply.github.com>
2022-05-22 13:34:59 +0300

    Added animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,22 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    private Animator _animator;
     private float _horizontalMove;
     [SerializeField] private float speed = 40f;
     private bool jump = false;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         _horizontalMove = CrossPlatformInputManager.GetAxisRaw("Horizontal") * speed;
+        
+        _animator.SetFloat("Speed", Mathf.Abs(_horizontalMove));
+        
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             jump = true;
